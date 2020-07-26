5a1b9dd440d80699f27b6ef1dd6ae51c2c9daf03
Naufal <mohammadnp2000@gmail.com>
2020-07-27 05:50:19 +0700

    first commit

--- app/src/main/java/com/example/ownyourshoes/About.kt
+++ app/src/main/java/com/example/ownyourshoes/About.kt
@@ -7,5 +7,15 @@ class About : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = "About Me"
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- app/src/main/java/com/example/ownyourshoes/DetailActivity.kt
+++ app/src/main/java/com/example/ownyourshoes/DetailActivity.kt
@@ -5,12 +5,13 @@ import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_detail.*
 
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(), View.OnClickListener {
 
 
     companion object{
@@ -41,6 +42,26 @@ class DetailActivity : AppCompatActivity() {
         detailActivityname.text = name //Untuk menampilkan judul
         detailActivitydetail.text = detail //Untuk menampilkan detail
         detailActivityprice.text = price //Untuk menampilkan price
+
+        keranjang.setOnClickListener(this)
+        beli.setOnClickListener(this)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = ""
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.keranjang -> Toast.makeText(this, "Barang telah dimasukkan dalam keranjang", Toast.LENGTH_LONG).show()
+            R.id.beli -> Toast.makeText(this, "Barang berhasil dibeli", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 
 }
